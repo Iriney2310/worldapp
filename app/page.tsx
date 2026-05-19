1b06f4d2983f67d0fa30ff84d59e3b0b034b138f
Iriney <irineymm@gmail.com>
2026-05-19 17:15:06 +0200

    sneaker app update

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
             background: 'transparent',
             color: 'white',
             cursor: 'pointer',
-            fontSize: 16,
+            fontSize: 18,
           }}
         >
           ☰
