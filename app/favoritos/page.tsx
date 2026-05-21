6acbbf2095b43bf4e3876e3b15510ede3a7b612b
Iriney <irineymm@gmail.com>
2026-05-21 11:05:14 +0200

    sneaker app update

---
+++ app/favoritos/page.tsx
@@ -0,0 +1,8 @@
+export default function Favoritos() {
+  return (
+    <div style={{ color: 'white', padding: 20 }}>
+      <h1>❤️ Favoritos</h1>
+      <p>Aquí verás tus zapatillas guardadas</p>
+    </div>
+  )
+}
