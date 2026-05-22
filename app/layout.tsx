396bcf94b0958b48970a0aef78b9ad7ec1754e6f
Iriney <irineymm@gmail.com>
2026-05-22 13:48:52 +0200

    sneaker app update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,14 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
-import { useEffect, useState } from "react"
 import BottomNav from "./bottomnav"
-
-const [darkMode, setDarkMode] = useState(true)
-
-useEffect(() => {
-  const theme = localStorage.getItem("theme")
-
-  if (theme === "light") {
-    document.getElementById("app-body")?.classList.add("light")
-  }
-}, [])
-
-useEffect(() => {
-  const theme = localStorage.getItem("theme")
-
-  if (theme === "light") {
-    document.getElementById("app-body")?.classList.add("light")
-  }
-}, [])
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 })
 
-const geistMono = Geist({
+const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 })
@@ -44,7 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body id="app-body" style={{ paddingBottom: 80 }}>
+      <body style={{ paddingBottom: 80 }}>
         {children}
         <BottomNav />
       </body>
