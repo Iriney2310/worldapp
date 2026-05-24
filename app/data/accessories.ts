8f76fc0c6f044476889b656b3bf94477fe20d9aa
Iriney <irineymm@gmail.com>
2026-05-24 16:04:20 +0200

    update

--- app/data/accessories.ts
+++ app/data/accessories.ts
@@ -22,7 +22,7 @@ export const accessories: Accessory[] = [
 
   {
     id: 2,
-    name: "WalletIcon AirMax 1",
+    name: "Wallet Icon AirMax 1",
     brand: "Nike",
     price: 24.67,
     image:
@@ -30,4 +30,30 @@ export const accessories: Accessory[] = [
     link: "https://meli.la/19WCXRZ",
     store: "MercadoLibre"
   },
+
+  {
+    id: 3,
+    name: "WalletIcon AirMax 90",
+    brand: "Nike",
+    price: 29.07,
+    image:
+      "https://static.luck-d.com/product/43069/main_carousel/NIKE_ICON_AIR_MAX_90_CARD_WALLET_INFRARED_HF3717068_130728.webp",
+    link: "https://meli.la/1LdeM7Y",
+    store: "MercadoLibre"
+  },
+
+  {
+    id: 4,
+    name: "WalletIcon AirMax 1",
+    brand: "Nike",
+    price: 27.13,
+    image:
+      "https://www.bing.com/images/blob?bcid=RH3U1vu.iOoJim3KLwR2rScZYoME.....4w",
+    link: "https://meli.la/2UdsTmM",
+    store: "MercadoLibre"
+  },
 ]
+
+
+
+
